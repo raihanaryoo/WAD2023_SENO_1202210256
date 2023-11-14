460c53135cdb0e3cd416ba6867d1bed32d459fc5
raihanaryoo <116295474+raihanaryoo@users.noreply.github.com>
2023-11-14 11:46:44 +0700

    Add files via upload

---
+++ MODUL3_SENO/connect.php
@@ -0,0 +1,13 @@
+<!-- File ini berisi koneksi dengan database yang telah di import ke phpMyAdmin kalian -->
+
+
+<?php
+// Buatlah variable untuk connect ke database yang telah di import ke phpMyAdmin
+$conn = mysqli_connect("localhost:3306", "root", "", "wad_modul3_seno");
+echo "<br/>";
+// 
+  
+// Buatlah perkondisian jika tidak bisa terkoneksi ke database maka akan mengeluarkan errornya
+
+// 
+?>
---
+++ MODUL3_SENO/list_mobil.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List Mobil</title>
+</head>
+<body>
+    <?php include("navbar.php") ?>
+    <center>
+        <div class="container">
+            <h1>List Mobil</h1>
+
+            <?php
+            include("connect.php");
+
+            // Buatlah query untuk mengambil data dari database (gunakan query SELECT)
+            $result = mysqli_query($conn, "SELECT * FROM showroom_mobil ");
+            
+            
+
+            // Buatlah perkondisian dimana: 
+            // 1. a. Apabila ada data dalam database, maka outputnya adalah semua data dalam database akan ditampilkan 
+            //       (gunakan num_rows > 0, while, dan mysqli_fetch_assoc())
+            //    b. Untuk setiap data yang ditampilkan, buatlah sebuah button atau link yang akan mengarahkan web ke halaman 
+            //       form_detail_mobil.php dimana halaman tersebut akan menunjukkan seluruh data dari satu mobil berdasarkan id
+            // 2. Apabila tidak ada data dalam database, maka outputnya adalah pesan 'tidak ada data dalam tabel'
+
+            //<!--  **********************  1  **************************     -->
+            if($result){
+                while($selects = mysqli_fetch_assoc($query)){
+            ?>
+            <tr>
+                <th scope="row"><?= $selects['id']?></th>
+                <td><?= $selects['nama_mobil']?></td>
+                <td><?= $selects['brand_mobil']?></td>
+                <td><?= $selects['warna_mobil']?></td>
+                <td><?= $selects['tipe_mobil']?></td>
+                <td><?= $selects['harga_mobil']?></td>
+            </tr>
+            <?php
+                    }
+                }
+            ?>
+
+
+
+
+
+
+            //<!--  **********************  1  **************************     -->
+
+            //<!--  **********************  2  **************************     -->
+
+            
+            
+
+            
+            
+            //<!--  **********************  2  **************************     -->
+            ?>
+        </div>
+    </center>
+</body>
+</html>
