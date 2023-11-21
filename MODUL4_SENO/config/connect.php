5416f846ceb2747112dc4d0f6221230757e4222d
raihanaryoo <raihanaryo92@gmail.com>
2023-11-21 12:11:28 +0700

    MODUL4

---
+++ MODUL4_SENO/config/connect.php
@@ -0,0 +1,13 @@
+<!-- File ini berisi koneksi dengan database MySQL -->
+<?php 
+
+// (1) Buatlah variable untuk connect ke database yang telah di import ke phpMyAdmin
+$conn = mysqli_connect("localhost:80", "root", "", "MODUL4_SENO");
+echo "<br/>";
+// 
+
+// (2) Buatlah perkondisian untuk menampilkan pesan error ketika database gagal terkoneksi
+
+// 
+ 
+?>
